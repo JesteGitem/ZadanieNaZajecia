b04f6d09facaa3ee5d23fec1069abdd4a000a624
michał wawoczny <michaltoja@outlook.com>
2020-01-26 23:54:48 +0100

    Update Program.cs

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -6,13 +6,7 @@ namespace zadanie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-
-
-            Console.WriteLine("Hello World!");
-
-            Console.WriteLine("Hello World!");
+   
 
         }
     }
